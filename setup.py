c1d7cb2903e007d65461e1a7f9a9c03157acf498
Marc <marc@marc.local>
2015-03-21 18:37:59 +0100

    Add project skeleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'TODO',
+    'author': 'Marc',
+    'url': 'https://github.com/mbrc/hamster',
+    'version': '0.0.1',
+    'install_requires': ['nose'],
+    'packages': ['hamster'],
+    'scripts': [],
+    'name': 'Hamster'
+}
+
+setup(**config)
---
+++ tests/hamster_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+import hamster
+
+def setup():
+    print('SETUP')
+
+def teardown():
+    print('TEAR DOWN')
+
+def test_basic():
+    print('BASIC TEST')
