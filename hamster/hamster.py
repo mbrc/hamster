7f13ad0d8ff20042db2871887568b0eb74cf7e31
mbrc <marc.sommerhalder@bluewin.ch>
2015-03-25 22:45:48 +0100

    Initialize database in main script

--- hamster/hamster.py
+++ hamster/hamster.py
@@ -1,10 +1,16 @@
 # TODO: Write a file comment.
 
+import db
 from xmlimport import XMLImporter
 
 def main():
+    # TODO: Implement option to recreate the database by passing a command line argument
+    if not db.is_sqlite3(db.DATABASE_PATH):
+        db.create_schema()
+
     importer = XMLImporter()
     library = importer.import_xml('library.xml')
+    print(type(library))
 
 if __name__ == '__main__':
     main()
