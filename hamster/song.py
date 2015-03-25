66d9357219ef28d485e040da4782832310a803f5
mbrc <marc.sommerhalder@bluewin.ch>
2015-03-25 22:36:07 +0100

    Remove obsolete classes (they might be reimplemented as soon as I've become smarter)

--- hamster/song.py
+++
@@ -1,9 +0,0 @@
-# TODO: Write a file comment.
-
-class Song:
-    name = None
-    track_number = None
-
-    def __init__(self, name, track_number):
-        self.name = name
-        self.track_number = track_number
